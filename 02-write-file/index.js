588b6d90c328cff9c71ca35b576b3143ff456f3b
Professional <sania_38@list.ru>
2022-05-25 16:15:12 +0600

    fix: html-builder task 2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,10 +1,10 @@
 const { stdin, stdout, exit } = process;
 const fs = require('fs');
-const output = fs.createWriteStream('02-write-file/output.txt');
+const output = fs.createWriteStream('output.txt');
 stdout.write('Привет, введи любой текст\n');
 stdin.on('data', data => {
   if (data.toString().trim() === 'exit') exit();
   output.write(data);
-  process.exit();
 });
 process.on('exit', () => stdout.write('Пока!'));
+process.on('SIGINT', exit);
